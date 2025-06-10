e0098ad73373192bf080791e397ad4970555997b
Prerith_shetty <123531490+prerith64@users.noreply.github.com>
2025-06-10 10:37:49 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-   const api = axios.create({baseURL:import.meta.env.BACKEND_URL,
+   const api = axios.create({baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials:true
 })
 
