c79e050cbc74ef2c42f287a300c00b0586be6e5a
Prerith_shetty <123531490+prerith64@users.noreply.github.com>
2025-06-10 12:02:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,12 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.FRONTEND_URL , // or your frontend domain
+  origin: FRONTEND_URL, // or your frontend domain
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
