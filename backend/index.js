d181911aa1283e93a6c59fdd30e574986535d7bf
Prerith_shetty <123531490+prerith64@users.noreply.github.com>
2025-06-10 11:46:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.FRONTEND_URL, // or your frontend domain
+  origin: process.env.FRONTEND_URL , // or your frontend domain
   credentials: true, // allow cookies
 }));
 
